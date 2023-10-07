c667231370f2bc3c5559bd5849f13c0da8034c23
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-10-07 22:59:15 +0530

    updated the nasa js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const ejs = require('ejs');
 
 const app = express();
 const PORT = 5000;
@@ -32,18 +31,18 @@ app.get('/', (req, res) => {
   res.send('Welcome to the NASA API');
 });
 
-// Route to render a page for the collection_data_inventory collection
+// Route to retrieve data from the nakamura_1979_sm_locations collection
 app.get('/nakamura_1979_sm_locations', async (req, res) => {
-    try {
-        // Access the collection directly using mongoose.connection
-        const documents = await mongoose.connection.db.collection('nakamura_1979_sm_locations').find({}, { _id: 0 }).toArray();
-    
-        // Send the extracted data for all documents as a JSON response
-        res.json(documents);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).send('Internal Server Error');
-      }
+  try {
+    // Access the collection directly using mongoose.connection
+    const documents = await mongoose.connection.db.collection('nakamura_1979_sm_locations').find({}, { _id: 0 }).toArray();
+
+    // Send the extracted data for all documents as a JSON response
+    res.json(documents);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // ... rest of your code
